c11dec5733bd8ddcc00c17d5804ff1adabd3a845
Кузьмичева Анна <Annrol60@gmail.com>
2026-05-20 23:06:17 +0300

    Лаб раб 3

---
+++ задача_3.ts
@@ -0,0 +1,17 @@
+const a = 0.05;
+const b = 0.06;
+
+// Задача А
+console.log(Math.acos(0.2*0.2 - 0.06*0.06) / Math.asin(0.2*0.2 - 0.05*0.05));
+console.log(Math.acos(0.35*0.35 - 0.06*0.06) / Math.asin(0.35*0.35 - 0.05*0.05));
+console.log(Math.acos(0.5*0.5 - 0.06*0.06) / Math.asin(0.5*0.5 - 0.05*0.05));
+console.log(Math.acos(0.65*0.65 - 0.06*0.06) / Math.asin(0.65*0.65 - 0.05*0.05));
+console.log(Math.acos(0.8*0.8 - 0.06*0.06) / Math.asin(0.8*0.8 - 0.05*0.05));
+console.log(Math.acos(0.95*0.95 - 0.06*0.06) / Math.asin(0.95*0.95 - 0.05*0.05));
+
+// Задача Б
+console.log(Math.acos(0.15*0.15 - 0.06*0.06) / Math.asin(0.15*0.15 - 0.05*0.05));
+console.log(Math.acos(0.26*0.26 - 0.06*0.06) / Math.asin(0.26*0.26 - 0.05*0.05));
+console.log(Math.acos(0.37*0.37 - 0.06*0.06) / Math.asin(0.37*0.37 - 0.05*0.05));
+console.log(Math.acos(0.48*0.48 - 0.06*0.06) / Math.asin(0.48*0.48 - 0.05*0.05));
+console.log(Math.acos(0.56*0.56 - 0.06*0.06) / Math.asin(0.56*0.56 - 0.05*0.05));
