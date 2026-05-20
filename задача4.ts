5d7c8a643accf6414018c71106c2d5811c0b848c
Кузьмичева Анна <Annrol60@gmail.com>
2026-05-20 23:31:31 +0300

    Лаб раб 4

---
+++ задача4.ts
@@ -0,0 +1,20 @@
+const a = 0.05;
+const b = 0.06;
+
+function calculate(x) {
+    return Math.acos(x * x - b * b) / Math.asin(x * x - a * a);
+}
+
+// Задача А
+const taskA = [0.20, 0.35, 0.50, 0.65, 0.80, 0.95];
+console.log("Задача А:");
+for (let i = 0; i < taskA.length; i++) {
+    console.log(calculate(taskA[i]));
+}
+
+// Задача Б
+const taskB = [0.15, 0.26, 0.37, 0.48, 0.56];
+console.log("Задача Б:");
+for (let i = 0; i < taskB.length; i++) {
+    console.log(calculate(taskB[i]));
+}
